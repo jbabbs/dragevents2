becc3aec5b5247e499ae0bc23d919b9c0a131e49
Justin Babbs <justin.babbs@gmail.com>
2015-07-12 20:40:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dragevents2
---
+++ index.js
@@ -0,0 +1,60 @@
+/*
+ * Usage: var elem = getElementById('someId');
+ *        var de2 = new DragEvents2(elem);
+ *        elem.addEventListener('dragenter2', function() {...});
+ *        elem.addEventListener('dragleave2', function() {...});
+ *        // if you wish to unbind 'dragenter2' and 'dragleave2'
+ *        de2.unbindEvents();
+ */
+(function() {
+  if (typeof module === 'object') {
+    module.exports = DragEvents2;
+  } else {
+    window['DragEvents2'] = DragEvents2;
+  }
+
+  var targetEnter = function(e) {
+    if (e.target !== this.target || this.alreadyEntered) {
+      this.disableLeave = true;
+      return;
+    }
+    this.alreadyEntered = true;
+    this.target.dispatchEvent(new Event('dragenter2'));
+  };
+
+  var targetLeave = function(e) {
+    if (this.disableLeave) {
+      this.disableLeave = false;
+      return;
+    }
+    this.alreadyEntered = false;
+    this.target.dispatchEvent(new Event('dragleave2'));
+  };
+
+  var targetDrop = function(e) {
+    this.alreadyEntered = false;
+  };
+
+  function unbindEvents() {
+    this.target = null;
+    this.target.removeEventListener('dragenter', targetEnter.bind(this));
+    this.target.removeEventListener('dragleave', targetLeave.bind(this));
+    this.target.removeEventListener('drop', targetDrop.bind(this));
+  }
+
+  function bindEvents() {
+    this.target.addEventListener('dragenter', targetEnter.bind(this), true);
+    this.target.addEventListener('dragleave', targetLeave.bind(this), true);
+    this.target.addEventListener('drop', targetDrop.bind(this), true);
+  }
+
+  function DragEvents2(e) {
+    this.target = e;
+    this.disableLeave = false;
+    this.alreadyEntered = false;
+    this.bindEvents();
+  }
+
+  DragEvents2.prototype.bindEvents = bindEvents;
+  DragEvents2.prototype.unbindEvents = unbindEvents;
+})();
